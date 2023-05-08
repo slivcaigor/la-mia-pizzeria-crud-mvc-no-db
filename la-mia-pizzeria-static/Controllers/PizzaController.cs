c4f050de2e4f2b8e3e5aa897c534d940773c1530
slivcaigor <slivcaigor@gmail.com>
2023-05-08 16:40:54 +0200

    Refactoring + CRUD

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -4,29 +4,172 @@ using Microsoft.AspNetCore.Mvc;
 namespace la_mia_pizzeria_static.Controllers
 {
     public class PizzaController : Controller
-    { 
+    {
+        public static List<Pizza> PizzaListings = new List<Pizza> {
+            new Pizza {
+                Id = 1,
+                Name = "Chicken Alfredo Pizza",
+                Image = "/img/Chicken Alfredo Pizza.png",
+                Description = "Alfredo sauce, cooked chicken, sliced mushrooms, mozzarella cheese, and fresh parsley",
+                Price = 13.10,
+                IsNew = true
+            },
+            new Pizza {
+                Id = 2,
+                Name = "Mushroom Pizza",
+                Image = "/img/Funghi.png",
+                Description = "Tomato sauce, mozzarella cheese, Italian sausage, sliced mushrooms, and fresh oregano",
+                Price = 10.24,
+                IsNew = false
+            },
+            new Pizza {
+                Id = 3,
+                Name = "Ham Pizza",
+                Image = "/img/Ham.png",
+                Description = "Pepperoni Pizza: Tomato sauce, mozzarella cheese, sliced ham, and oregano.",
+                Price = 12.14,
+                IsNew = false
+            },
+            new Pizza {
+                Id = 4,
+                Name = "Margherita Pizza",
+                Image = "/img/MargheritaPizza.png",
+                Description = "San Marzano tomatoes, fresh mozzarella cheese, fresh basil leaves, extra-virgin olive oil, and salt",
+                Price = 9.64,
+                IsNew = false
+            },
+            new Pizza {
+                Id = 5,
+                Name = "Meat Lovers Pizza",
+                Image = "/img/Meat.png",
+                Description = "Tomato sauce, mozzarella cheese, Italian sausage, bacon, pepperoni, and ham",
+                Price = 14.87,
+                IsNew = true
+            },
+            new Pizza {
+                Id = 6,
+                Name = "Pesto Pizza",
+                Image = "/img/Pesto.png",
+                Description = "Pesto sauce, sliced tomatoes, mozzarella cheese, and sliced black olives",
+                Price = 11.24,
+                IsNew = true
+            },
+            new Pizza {
+                Id = 7,
+                Name = "Tuna Pizza",
+                Image = "/img/Tuna.png",
+                Description = "Olive oil, mozzarella cheese, ricotta cheese, garlic, and fresh parsley and tuna",
+                Price = 10.64,
+                IsNew = false
+            },
+            new Pizza {
+                Id = 8,
+                Name = "Veggie Pizza",
+                Image = "/img/Veggie Pizza.png",
+                Description = "Tomato sauce, mozzarella cheese, green bell pepper, red onion, sliced mushrooms, and sliced black olives",
+                Price = 12.14,
+                IsNew = false
+            },
+            new Pizza {
+                Id = 9,
+                Name = "Buffalo Chicken Pizza",
+                Image = "/img/BuffaloChickenPizza.png",
+                Description = "Buffalo sauce, cooked chicken, blue cheese, mozzarella cheese, and sliced celery",
+                Price = 13.44,
+                IsNew = true
+            }
+        };
+
         public IActionResult Details(int id)
         {
-            var listing = Menu.PizzaListings.FirstOrDefault(l => l.Id == id);
+            var pizza = PizzaListings.FirstOrDefault(l => l.Id == id);
 
-            if (id < 1 || id > Menu.PizzaListings.Count)
+            if (id < 1 || id > PizzaListings.Count)
             {
                 return View("Error404");
             }
-            return View(listing);
+            return View(pizza);
         }
 
         public IActionResult Index()
         {
-            var listings = Menu.PizzaListings;
-            return View(listings);
+            return View(PizzaListings);
         }
 
         [HttpGet]
-        public IActionResult Create(string type)
+        public IActionResult Create()
         {
-            ViewData["Type"] = type;
-            return View();
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pizza);
+            }
+
+            pizza.Id = PizzaListings.Max(p => p.Id) + 1;
+            PizzaListings.Add(pizza);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Pizza pizza = PizzaListings.FirstOrDefault(p => p.Id == id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Pizza data)
+        {
+
+            if (!ModelState.IsValid)
+                return View("Edit", data);
+
+            Pizza pizzaEdit = PizzaListings.Where(p => p.Id == id).FirstOrDefault();
+
+            if (pizzaEdit != null)
+            {
+                pizzaEdit.Name = data.Name;
+                pizzaEdit.Description = data.Description;
+                pizzaEdit.Price = data.Price;
+                pizzaEdit.Image = data.Image;
+
+                PizzaListings.Add(data);
+                return RedirectToAction("Index");
+            }
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            Pizza pizzaDelete = PizzaListings.Where(p => p.Id == id).FirstOrDefault();
+
+            if (pizzaDelete != null)
+            {
+                PizzaListings.Remove(pizzaDelete);
+                return RedirectToAction("Index", new { message = "Pizza eliminated successfully!" });
+            }
+            else
+                return NotFound();
         }
     }
 }
--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -2,29 +2,28 @@
 
 namespace la_mia_pizzeria_static.Models
 {
-    public class Listing
+    public class Pizza
     {
-        [Required(ErrorMessage = "Please enter the id of the listing.")]
+        [Required(ErrorMessage = "Please enter the id of the pizza.")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Please enter the name of the listing.")]
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "The listing name must be between 5 and 50 characters.")]
+        [Required(ErrorMessage = "Please enter the name of the pizza.")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "The pizza name must be between 5 and 50 characters.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter the image URL of the listing.")]
+        [Required(ErrorMessage = "Please enter the image URL of the pizza.")]
         public string Image { get; set; }
 
-        [Required(ErrorMessage = "Please enter a description of the listing.")]
+        [Required(ErrorMessage = "Please enter a description of the pizza.")]
         [MinWords(5, ErrorMessage = "The description must contain at least 5 words.")]
         public string Description { get; set; }
 
-        [Required(ErrorMessage = "Please enter the price of the listing.")]
+        [Required(ErrorMessage = "Please enter the price of the pizza.")]
         [Range(1, 100, ErrorMessage = "The price must be between 1 and 100.")]
         public double Price { get; set; }
 
         public bool IsNew { get; set; }
 
-        public string Type { get; set; }
     }
 
     public class MinWordsAttribute : ValidationAttribute
